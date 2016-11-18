ff42803afa38e8657ebefb20ce245aa322a5e8a1
Andrii Dychka <at4293@c127-10.enst.fr>
2016-11-18 10:43:46 +0100

    lines

--- sample.m
+++ sample.m
@@ -8,33 +8,33 @@ img=imread('images/page1_BW.jpg');
 ima = img;
 colormap(gray(256));
 figure();
-image(ima);
-figure(2);
-ima = binarize(ima);
-[x,y] = size(ima);
-disp(y);
-disp(x);
-ima = inverse_bin(ima);
-ima = ero(buildStructuralElementForDim(1),ima);
-ima = dil(buildStructuralElementForDim(7),ima);
-ima = ero(buildStructuralElementForDim(8),ima);
-lines = findLineBounds(ima,0.001,10);
-lines = cutZeros(lines);
-lines = extendBorders(lines,5);
-ima = back_inverse_bin(ima);
-image(ima);
-images = getByLines(img,lines);
-colormap(gray(256));
-disp(size(images));
-[x,y] = size(images);
-
-for i = 1 : y
-    try
-    s = sprintf('images/line %i.png', i);
-    imwrite(images{i},s);
-    catch
-    end;
-end
-
-l = im2bw(imread('images/line 1.png'),0.9);
-disp(wordcountinlines(l));
+image(transpose(ima));
+% figure(2);
+% ima = binarize(ima);
+% [x,y] = size(ima);
+% disp(y);
+% disp(x);
+% ima = inverse_bin(ima);
+% ima = ero(buildStructuralElementForDim(1),ima);
+% ima = dil(buildStructuralElementForDim(7),ima);
+% ima = ero(buildStructuralElementForDim(7),ima);
+% lines = findLineBounds(ima,0.001,10);
+% lines = cutZeros(lines);
+% lines = extendBorders(lines,5);
+% ima = back_inverse_bin(ima);
+% image(ima);
+% images = getByLines(img,lines);
+% colormap(gray(256));
+% disp(size(images));
+% [x,y] = size(images);
+% 
+% for i = 1 : y
+%     try
+%     s = sprintf('images/line %i.png', i);
+%     imwrite(images{i},s);
+%     catch
+%     end;
+% end
+% 
+% l = im2bw(imread('images/line 1.png'),0.9);
+% disp(wordcountinlines(l));
